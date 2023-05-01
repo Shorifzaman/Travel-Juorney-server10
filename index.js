34258a9e7f5ca8df7bcf1874ba6af9a1be9317f5
shorifzaman <shorif.zaman@gmail.com>
2023-05-02 02:29:47 +0600

    [ADD] modify and others

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cors = require("cors");
 
 
 const port = process.env.PORT || 5000;
 // http://localhost:5000
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.json());
